bd4c3cda20152e1192fb4dfe0268743d0aab9c6b
friidger <amitisaacs2012@gmail.com>
2026-06-29 12:28:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+function getInput(){
+    let userInput = prompt("Pleas Write A Number Pookie Dooike ");
+    for(let i = 0; i<=userInput; i++){
+    if(i % 2 ==0){
+        document.writeln("<div class =header>"+ i+ "</div>");
+    }
+    x = Math.sqrt();
+}
+    alert("Done Pookie Dookie");
+}
+
+
+
+
+
+
+
+getInput();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
