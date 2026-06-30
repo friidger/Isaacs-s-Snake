36d0fb2ec09ec0d287d586b694d9745674b5a601
friidger <amitisaacs2012@gmail.com>
2026-06-30 11:33:09 +0300

    Remove old input function and implement snake game

--- script.js
+++ script.js
@@ -1,62 +1,55 @@
-function getInput(){
-    let userInput = prompt("Pleas Write A Number Pookie Dooike ");
-    for(let i = 0; i<=userInput; i++){
-    if(i % 2 ==0){
-        document.writeln("<div class =header>"+ i+ "</div>");
+const grid = document.getElementById('grid');
+const scoreDisplay = document.getElementById('score');
+let squares = [];
+let currentSnake = [2, 1, 0];
+let direction = 1;
+let appleIndex = 0;
+let score = 0;
+let timerId = 0;
+let intervalTime = 200;
+
+// יצירת הלוח (400 משבצות
+function createBoard() {
+    for (let i = 0; i < 400; i++) {
+        const square = document.createElement('div');
+        grid.appendChild(square);
+        squares.push(square)
     }
-    x = Math.sqrt();
 }
-    alert("Done Pookie Dookie");
+createBoard();
+startGame();
+generateApple();
+
+function startGame() {
+    currentSnake.forEach(index => squares[index].classList.remove('snake'));
+    squares[appleIndex].classList.remove('apple');
+    clearInterval(timerId);
 }
 
+function move() {
+    const hitBottom = (currentSnake[0] + 20 >= 400 && direction === 20);
+    const hitTop = (currentSnake[0] - 20 < 0 && direction === -20);
+    const hitRight = (currentSnake[0] % 20 === 19 && direction === 1);
+    const hitLeft = (currentSnake[0] % 20 === 0 && direction === -1);
+    const hitSelf = squares[currentSnake[0] + direction]?.classList.contains('snake');
+}
 
+function generateApple() {
+    do {
+        appleIndex = Math.floor(Math.random() * squares.length);
+    } while (squares[appleIndex].classList.contains('snake'));
+    squares[appleIndex].classList.add('apple');
+}
 
+function changeDir(newDir) {
+    if (direction + newDir !== 0) {
+        direction = newDir;
+    }
+}
 
-
-
-
-getInput();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowUp') changeDir(-20);
+    if (e.key === 'ArrowDown') changeDir(20);
+    if (e.key === 'ArrowLeft') changeDir(1);
+    if (e.key === 'ArrowRight') changeDir(-1);
+});
